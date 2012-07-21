491b02efa2d3ab206d61e001354fe5a008622e7b
Kirill Lebedev <echo.bjornd@gmail.com>
2012-07-21 11:12:00 +0300

    fixes #31 Added improved markers API

--- jquery-jvectormap.js
+++ jquery-jvectormap.js
@@ -37,55 +37,7 @@
       };
 
   $.fn.vectorMap = function(options) {
-    var defaultParams = {
-          map: 'world_mill_en',
-          backgroundColor: '#505050',
-          scaleColors: ['#b6d6ff', '#005ace'],
-          normalizeFunction: 'linear',
-          zoomOnScroll: true,
-          zoomMax: 8,
-          zoomMin: 1,
-          zoomStep: 1.6,
-          regionsSelectable: false,
-          markersSelectable: false,
-          regionStyle: {
-            initial: {
-              fill: 'white',
-              "fill-opacity": 1,
-              stroke: 'none',
-              "stroke-width": 0,
-              "stroke-opacity": 1
-            },
-            hover: {
-              "fill-opacity": 0.8
-            },
-            selected: {
-              fill: 'yellow'
-            },
-            selectedHover: {
-            }
-          },
-          markerStyle: {
-            initial: {
-              fill: 'white',
-              stroke: '#505050',
-              "fill-opacity": 1,
-              "stroke-width": 1,
-              "stroke-opacity": 1,
-              r: 5
-            },
-            hover: {
-              stroke: 'black',
-              "stroke-width": 2
-            },
-            selected: {
-              fill: 'blue'
-            },
-            selectedHover: {
-            }
-          }
-        },
-        map,
+    var map,
         methodName,
         event;
 
@@ -95,13 +47,12 @@
       methodName = arguments[1].charAt(0).toUpperCase()+arguments[1].substr(1);
       return this.data('mapObject')[options+methodName].apply(this.data('mapObject'), Array.prototype.slice.call(arguments, 2));
     } else {
-      $.extend(true, defaultParams, options);
-      defaultParams.container = this;
+      options.container = this;
       this.css({
         position: 'relative',
         overflow: 'hidden'
       });
-      map = new jvm.WorldMap(defaultParams);
+      map = new jvm.WorldMap(options);
       this.data('mapObject', map);
       for (event in apiEvents) {
         if (defaultParams[event]) {
